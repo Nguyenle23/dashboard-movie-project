0f5d43445ef073dcb27e86173d47fbd704938201
Nguyenle23___ <beenbin2308@gmail.com>
2022-11-07 19:57:43 +0700

    Build | Build Error handler & implement to MovieController

--- server-dashboard/src/main/java/com/server/netflix/repositories/MovieServiceRepository.java
+++ server-dashboard/src/main/java/com/server/netflix/repositories/MovieServiceRepository.java
@@ -1,7 +1,9 @@
 package com.server.netflix.repositories;
 
+import com.server.netflix.config.ResourceNotFoundException;
 import com.server.netflix.models.Movie;
 import org.bson.types.ObjectId;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +13,7 @@ public interface MovieServiceRepository {
 
     List<Movie> getAllMovies();
 
-    Optional<Movie> getMovieById(String id);
+    ResponseEntity<Movie> getMovieById(String id) throws ResourceNotFoundException;
 
     void updateMovie(Movie movie);
 
